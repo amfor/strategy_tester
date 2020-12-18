0afe1936bca4d7c4c6e83315461efd49abe147c6
‘Amy <aryawa@icloud.com>
2020-12-18 17:31:39 -0500

    Added logic for DCA buying & preliminary streamlit pages; added sell side logic; reformatted streamlit project to use column containers;

--- st_streamlit.py
+++ st_streamlit.py
@@ -10,15 +10,65 @@ import trade_logic
 def load_ticker(ticker):
     ticker_obj = yf.Ticker(ticker)
     history = ticker_obj.history(period='max')
-    return history
+    details = ticker_obj.info
+    return history, details
 
+pages = ['Technical Trading', 'Dollar Cost Averaging']
+
+# Initial App Setup
 st.set_page_config('Strategy Tester', layout='wide')
-
-
+selected_page = st.radio("Buying Strategies", pages)
 st.sidebar.title('Strategy Tester')
 selected_ticker = st.sidebar.text_input('Input Your Ticker', 'MSFT')
-history = load_ticker(selected_ticker)
+st.sidebar.subheader(selected_page)
 
-strategy = st.sidebar.selectbox('Select Buying Strategy', list(trade_logic.strategies.keys()))
+history, info = load_ticker(selected_ticker) # Load in Data
 
-st.plotly_chart(plot_funcs.plot_buys(history, strategy=strategy, scaling=0.85), use_container_width=True, height= 900)
+
+if selected_page == pages[0]:
+
+    buy_col, sell_col = st.beta_columns(2)
+
+    with buy_col:
+        buy_strategy = st.selectbox('Select Buying Strategy', list(trade_logic.buy_strategies.keys()))
+        buy_strategy_span = st.number_input('Buy MA Span', value=25)
+
+    with sell_col:
+        sell_strategy = st.selectbox('Select Selling Strategy', list(trade_logic.sell_strategies.keys()))
+        sell_strategy_span = st.number_input('Sell MA Span', value=25)
+
+    fig = plot_funcs.plot_candlestick(history)
+
+    fig = plot_funcs.plot_buys(figure=fig,
+                               plot_data=history,
+                               strategy=buy_strategy,
+                               ma_span=buy_strategy_span,
+                               scaling=0.95)
+
+    fig = plot_funcs.plot_sells(figure=fig,
+                               plot_data=history,
+                               strategy=sell_strategy,
+                               ma_span=sell_strategy_span,
+                               scaling=1.05)
+
+    st.plotly_chart(fig, use_container_width=True, height=900)
+
+if selected_page == pages[1]:
+
+    weekdays = {'Monday': 0,
+                'Tuesday': 1,
+                'Wednesday': 2,
+                'Thursday': 3,
+                'Friday': 4}
+
+    time_strategy = ['Open', 'Close']
+
+    weekday_col, strategy_col, interval_col = st.beta_columns(3)
+
+    with weekday_col:
+        selected_weekday = st.selectbox('Purchase Day', list(weekdays.keys()))
+    with strategy_col:
+        selected_strategy = st.selectbox('Purchase Time', time_strategy)
+    with interval_col:
+        selected_interval = st.selectbox('Purchase Day', list(trade_logic.interval_strategy.keys()))
+
--- trade_logic.py
+++ trade_logic.py
@@ -1,24 +1,44 @@
 import pandas as pd
+pd.set_option('display.max_columns', 20)
+pd.set_option('display.width', 200)
+
 
 # Returns simple moving average with provided span
-def sma_line(series, span=200):
+def sma_line(series, span=None):
+    if span is None:
+        span = 200
     return series.rolling(window=span).mean()
 
 # Returns exponentially weighed average based on provided span
-def ema_line(series, span=14):
+def ema_line(series, span=None):
+    if span is None:
+        span = 25
     return series.ewm(span).mean()
 
-strategies = {'Buy on SMA': sma_line,
-              'Buy on EMA': ema_line}
+buy_strategies = {'Buy on SMA': sma_line,
+                  'Buy on EMA': ema_line,
+                  'Buy on SMA Crossover': sma_line}
+
+sell_strategies = {'Sell on SMA': sma_line,
+              'Sell on EMA': ema_line}
+
+interval_strategy = {'Weekly': 1,
+                 'Bi-Monthly': 2,
+                 'Monthly': 4}
 
 # Returns the buy date if the daiy price reaches the selected strategy's line.
 # Gap prevents buys within small timeframes. ie: Gap of 7 = maximum of a weekly buy frequency
 # Returns boolean series with Buy=1, No Buy=0 along with Respective Moving Average(s)
-def get_buy_point(strategy, asset_data, gap=0, scaling=1):
+def get_buy_point(asset_data, strategy, gap=0, span=None, scaling=1):
 
     # TODO: Should the buy point depend on the last day of data (hypothetical purchase date?)
     daily_low = asset_data['Low']
-    buy_point = strategies.get(strategy)(asset_data['Close']) * scaling
+
+    buy_point = buy_strategies.get(strategy)(asset_data['Close'], span) * scaling
+
+    crossover_sma1 = sma_line(asset_data['Close'], span=200)
+    crossover_sma2 = sma_line(asset_data['Close'], span=150)
+
 
     # Line 1 Intersects Line 2 between points 0 and 1 when l1_0 < l2_0 & l1_1 > l2_1
     crossover_line = ((daily_low < buy_point) & (daily_low.shift(1) > buy_point.shift(1))).astype(int)
@@ -31,3 +51,52 @@ def get_buy_point(strategy, asset_data, gap=0, scaling=1):
                              index=crossover_line.index)
     return crossover_line, buy_point
 
+
+# Returns the buy date if the daiy price reaches the selected strategy's line.
+# Gap prevents buys within small timeframes. ie: Gap of 7 = maximum of a weekly buy frequency
+# Returns boolean series with Buy=1, No Buy=0 along with Respective Moving Average(s)
+def get_sell_point(asset_data, strategy, gap=0, span=None, scaling=1):
+
+    # TODO: Should the buy point depend on the last day of data (hypothetical purchase date?)
+    daily_high = asset_data['High']
+
+    sell_point = sell_strategies.get(strategy)(asset_data['Close'], span) * scaling
+
+    # Line 1 Intersects Line 2 between points 0 and 1 when l1_0 < l2_0 & l1_1 > l2_1
+    crossover_line = ((daily_high > sell_point) & (daily_high.shift(1) < sell_point.shift(1))).astype(int)
+
+    # TODO: make compatible with date index
+    if gap > 0:
+        new_line = [value if (value == 1 and (1 != crossover_line.values[idx + 1: idx + gap + 1]).all())
+                                   else 0 for idx, value in zip(range(0, len(crossover_line)), crossover_line.values)]
+        crossover_line = pd.Series(data=new_line,
+                             index=crossover_line.index)
+    return crossover_line, sell_point
+
+# Returns series of -1, 0, 1 values. 1 = series_one crosses upward, -1 = series_two crosses upward, 0 = No Cross
+def get_crossover_point(series_one, series_two):
+
+    downward_cross = ((series_one > series_two) & (series_one.shift(1) < series_two.shift(1))).astype(int)
+    upward_cross = ((series_one < series_two) & (series_one.shift(1) > series_two.shift(1))).astype(int)
+
+    return upward_cross, downward_cross
+
+
+# Logic for a naive buying strategy on a specific weekday at certain intervals.
+# If the buy amount is < share price, the share quantity bought will be 1. Assumes no fractional shares.
+def dca_buy_point(asset_data, weekday, strategy, interval, usd_buy_amount, start=None, allow_fractional=False):
+
+
+    buy_dates = asset_data.index[asset_data.index.weekday == weekday][::interval]
+    buy_dates = buy_dates[buy_dates > start] if start is not None else buy_dates
+    buy_df = asset_data.loc[buy_dates, [strategy]]
+
+    prelim_share_amount = (usd_buy_amount / buy_df[strategy])
+    buy_df['Shares Bought'] = prelim_share_amount if allow_fractional else prelim_share_amount.astype(int).replace(0, 1)
+    buy_df['Balance'] = buy_df['Shares Bought'].expanding().apply(sum)
+    buy_df['Cost Basis'] = (buy_df['Shares Bought'] * buy_df[strategy]).expanding().apply(sum) / buy_df['Balance']
+    buy_df['Unrealized PNL'] = buy_df['Balance'] * (buy_df['Close'] - buy_df['Cost Basis'])
+    
+    buy_df['Cumulative Spend'] = (buy_df['Shares Bought'] * buy_df[strategy]).expanding().apply(sum)
+    buy_df['Value'] = buy_df['Balance'] * buy_df['Close']
+    buy_df['ROE %'] = (buy_df['Value'] / buy_df['Cumulative Spend'] - 1) * 100
